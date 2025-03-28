94114ad7c22754ccdedd4a728155f70e8432e550
weyu <985695055@qq.com>
2025-03-28 12:59:19 +0800

    change workload

--- libs/aliyun.sh
+++ libs/aliyun.sh
@@ -1,8 +1,8 @@
 install_aliyun_ossutil(){
-  if [ ! -f /usr/bin/ossutil ];then
+  if [ ! -f ./ossutil ];then
     wget -O ossutil-2.0.6-beta.01091200-linux-amd64.zip  https://gosspublic.alicdn.com/ossutil/v2-beta/2.0.6-beta.01091200/ossutil-2.0.6-beta.01091200-linux-amd64.zip
     unzip ossutil-2.0.6-beta.01091200-linux-amd64.zip
-    cp ossutil-2.0.6-beta.01091200-linux-amd64/ossutil /usr/bin/ossutil
-    chmod +x ossutil
+    cp ossutil-2.0.6-beta.01091200-linux-amd64/ossutil ./ossutil
+    chmod +x ./ossutil
   fi
 }
--- websitetoto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-source libs/aliyun.sh
-
-main(){
-    install_aliyun_ossutil
-}
-
-main;
