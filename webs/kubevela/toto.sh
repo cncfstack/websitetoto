10d4417e5ae63aad9c5429171944918886722bea
weyu <985695055@qq.com>
2025-03-28 14:33:14 +0800

    test

--- webs/kubevela/toto.sh
+++ webs/kubevela/toto.sh
@@ -1,26 +1,3 @@
-before_kubevela(){
-    npm install
-    sed -i "s|plugins:\s*\[|plugins: [()=>({name:'umami-analytics',injectHtmlTags:()=>({headTags:[{tagName:'script',attributes:{defer:true,src:'https://umami.cncfstack.com/script.js','data-website-id':'8c8b1d6d-a7fa-43ca-9933-db30591777e9'}}]})}),|g" docusaurus.config.js
-    cat ./docusaurus.config.js
-}
+#!/bin/bash
 
-after_kubevela(){
-    echo "npm build-----"
-
-    npm run build
-
-    ls -lh
-
-    echo "复制文件到OSS"
-    $OSSUTIL cp -fr build oss://cncfstack-kubevela 
-}
-
-
-toto(){
-    if cat .git/config  |grep '/kubevela/kubevela.github.io.git' ;then
-        echo "/kubevela/kubevela.github.io.git"
-        before_kubevela
-        find_and_sed
-        after_kubevela
-    fi
-}
+echo $1
