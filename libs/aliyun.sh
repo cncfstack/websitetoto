25bdfc1fb36a7e2e206fa3054cd99cc1254ed965
weyu <985695055@qq.com>
2025-03-28 11:02:36 +0800

    fix aliyun.sh

--- libs/aliyun.sh
+++ libs/aliyun.sh
@@ -1,5 +1,5 @@
 install_aliyun_ossutil(){
-  if [ ! -f /usr/bin/ossutil ];
+  if [ ! -f /usr/bin/ossutil ];then
     wget -O ossutil-2.0.6-beta.01091200-linux-amd64.zip  https://gosspublic.alicdn.com/ossutil/v2-beta/2.0.6-beta.01091200/ossutil-2.0.6-beta.01091200-linux-amd64.zip
     unzip ossutil-2.0.6-beta.01091200-linux-amd64.zip
     cp ossutil-2.0.6-beta.01091200-linux-amd64/ossutil /usr/bin/ossutil
