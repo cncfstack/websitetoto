68b0f66d76a477756cf4c221f5ad68953f8a8b21
weyu <985695055@qq.com>
2025-03-29 15:20:55 +0800

    fix hugo v66

--- libs/common.sh
+++ libs/common.sh
@@ -10,8 +10,8 @@ install_hugo(){
 
 install_hugo_v66(){
     echo "=============================================> 安装 Hugo V0.66.0"
-    wget -q https://github.com/gohugoio/hugo/releases/download/v0.66.0/hugo_extended_0.66.0_Linux-amd64.tar.gz
-    tar xf hugo_extended_0.66.0_linux-amd64.tar.gz
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.66.0/hugo_extended_0.66.0_Linux-64bit.tar.gz
+    tar xf hugo_extended_0.66.0_Linux-64bit.tar.gz
     sudo cp ./hugo /usr/bin/
     sudo chmod +x /usr/bin/hugo
 }
