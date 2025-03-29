810af70775933af3b8915ae49db4f8c5b26e81fc
weyu <985695055@qq.com>
2025-03-29 16:03:22 +0800

    add coredns

--- libs/common.sh
+++ libs/common.sh
@@ -16,6 +16,13 @@ install_hugo_v66(){
     sudo chmod +x /usr/bin/hugo
 }
 
+install_hugo_v100_2(){
+    echo "=============================================> 安装 Hugo V0.100.2"
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.100.2/hugo_extended_0.100.2_Linux-64bit.tar.gz
+    tar xf hugo_extended_0.100.2_Linux-64bit.tar.gz
+    sudo cp ./hugo /usr/bin/
+    sudo chmod +x /usr/bin/hugo
+}
 
 install_hugo_v120(){
     echo "=============================================> 安装 Hugo V0.120.0"
@@ -40,6 +47,8 @@ install_hugo_v145(){
     sudo cp ./hugo /usr/bin/
     sudo chmod +x /usr/bin/hugo
 }
+
+
 
 
 install_postcss(){
---
+++ webs/coredns/toto.sh
@@ -0,0 +1,43 @@
+workdir=$1
+initdir=$2
+
+source ${initdir}/libs/common.sh
+
+before_coredns_website(){
+    install_hugo_v100_2
+    install_postcss
+
+    # 添加网站访问统计
+    echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="9f78fcf9-90dc-4597-8fb8-a2b26aaf7656"></script>' >>  layouts/partials/favicons.html
+
+}
+
+after_coredns_website(){
+
+    #npm run build:production
+
+    mkdir output
+    hugo \
+    --destination ./output \
+    --cleanDestinationDir \
+    --minify \
+    --gc \
+    --enableGitInfo \
+    --baseURL https://coredns.cncfstack.com
+
+}
+
+save_return(){
+    ls -lha
+    echo "${workdir}/output&oss://cncfstack-coredns" > ${workdir}/ret-data
+}
+
+
+cd $workdir
+if cat .git/config  |grep '/coredns/coredns.io.git' ;then
+    echo "=============================================> 匹配到 coredns"
+    before_coredns_website
+    after_coredns_website
+    find_and_sed_v2 "./output"
+    save_return 
+fi
