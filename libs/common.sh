d1766bc509e96961d9cd04531e010d1bf54ca0f1
weyu <985695055@qq.com>
2025-03-29 16:21:53 +0800

    add jaeger

--- libs/common.sh
+++ libs/common.sh
@@ -40,6 +40,14 @@ install_hugo_v122(){
     sudo chmod +x /usr/bin/hugo
 }
 
+install_hugo_v143_1(){
+    echo "=============================================> 安装 Hugo V0.143.1"
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_extended_0.143.1_linux-amd64.tar.gz
+    tar xf hugo_extended_0.143.1_linux-amd64.tar.gz
+    sudo cp ./hugo /usr/bin/
+    sudo chmod +x /usr/bin/hugo
+}
+
 install_hugo_v145(){
     echo "=============================================> 安装 Hugo V0.145.0"
     wget -q https://github.com/gohugoio/hugo/releases/download/v0.145.0/hugo_extended_0.145.0_linux-amd64.tar.gz
---
+++ webs/jaeger/toto.sh
@@ -0,0 +1,45 @@
+workdir=$1
+initdir=$2
+
+source ${initdir}/libs/common.sh
+
+before_jaeger_website(){
+    install_hugo_v143_1
+    install_postcss
+
+    # 添加网站访问统计
+    echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="ad128657-afe1-4074-aa5e-279f72db2a62"></script>' >>  layouts/partials/favicons.html
+
+
+
+}
+
+after_jaeger_website(){
+
+    #command = "make netlify-production-build"
+
+    mkdir output
+    hugo \
+    --destination ./output \
+    --cleanDestinationDir \
+    --minify \
+    --gc \
+    --enableGitInfo \
+    --baseURL https://jaeger.cncfstack.com
+
+}
+
+save_return(){
+    ls -lha
+    echo "${workdir}/output&oss://cncfstack-jaeger" > ${workdir}/ret-data
+}
+
+
+cd $workdir
+if cat .git/config  |grep '/jaegertracing/documentation.git' ;then
+    echo "=============================================> 匹配到 jaeger"
+    before_jaeger_website
+    after_jaeger_website
+    find_and_sed_v2 "./output"
+    save_return 
+fi
