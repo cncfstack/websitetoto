597876ab48c315b3110d76cfc753884cf1693efe
weyu <985695055@qq.com>
2025-03-30 17:29:30 +0800

    add kubeflow

--- libs/common.sh
+++ libs/common.sh
@@ -68,6 +68,14 @@ install_hugo_v122(){
     sudo chmod +x /usr/bin/hugo
 }
 
+install_hugo_v124_1(){
+    echo "=============================================> 安装 Hugo V0.124.1"
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.124.1/hugo_extended_0.124.1_linux-amd64.tar.gz
+    tar xf hugo_extended_0.124.1_linux-amd64.tar.gz
+    sudo cp ./hugo /usr/bin/
+    sudo chmod +x /usr/bin/hugo
+}
+
 install_hugo_v143_1(){
     echo "=============================================> 安装 Hugo V0.143.1"
     wget -q https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_extended_0.143.1_linux-amd64.tar.gz
---
+++ webs/kubeflow/toto.sh
@@ -0,0 +1,44 @@
+workdir=$1
+initdir=$2
+
+source ${initdir}/libs/common.sh
+
+before_kubeflow_website(){
+    install_hugo_v124_1
+    npm install 
+    git submodule update --init --recursive
+    cd themes/docsy/ && npm install && cd -
+
+    # 添加网站访问统计
+    echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="bb40c254-9dfe-47ec-9935-ecfd7263cc97"></script>' >>  ./layouts/partials/favicons.html
+
+}
+
+after_kubeflow_website(){
+
+    mkdir output
+    hugo \
+    --destination ./output \
+    --cleanDestinationDir \
+    --minify \
+    --gc \
+    --enableGitInfo \
+    --baseURL https://kubeflow.cncfstack.com
+
+}
+
+save_return(){
+    ls -lha
+    pwd
+    echo "${workdir}/output&oss://cncfstack-kubeflow" > ${workdir}/ret-data
+}
+
+
+cd $workdir
+if cat .git/config  |grep '/kubeflow/docs.git' ;then
+    echo "=============================================> 匹配到 kubeflow"
+    before_kubeflow_website
+    after_kubeflow_website
+    find_and_sed_v2 "./output"
+    save_return 
+fi
