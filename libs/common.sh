ced6e21c6e0609c3c4af5bc383fba532dc0f5662
weyu <985695055@qq.com>
2025-03-30 20:29:53 +0800

    add notary

--- libs/common.sh
+++ libs/common.sh
@@ -22,14 +22,21 @@ install_hugo_v68_3(){
     sudo cp ./hugo /usr/bin/
     sudo chmod +x /usr/bin/hugo
 }
-install_hugo_v74(){
-    echo "=============================================> 安装 Hugo V0.74.0"
-    wget -q https://github.com/gohugoio/hugo/releases/download/v0.74.0/hugo_extended_0.74.0_Linux-64bit.tar.gz
-    tar xf hugo_extended_0.74.0_Linux-64bit.tar.gz
+install_hugo_v83_1(){
+    echo "=============================================> 安装 Hugo V0.83.1"
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.83.1/hugo_extended_0.83.1_Linux-64bit.tar.gz
+    tar xf hugo_extended_0.83.1_Linux-64bit.tar.gz
     sudo cp ./hugo /usr/bin/
     sudo chmod +x /usr/bin/hugo
 }
 
+install_hugo_v93_2(){
+    echo "=============================================> 安装 Hugo V0.93.2"
+    wget -q https://github.com/gohugoio/hugo/releases/download/v0.93.2/hugo_extended_0.93.2_Linux-64bit.tar.gz
+    tar xf hugo_extended_0.93.2_Linux-64bit.tar.gz
+    sudo cp ./hugo /usr/bin/
+    sudo chmod +x /usr/bin/hugo
+}
 
 install_hugo_v93_2(){
     echo "=============================================> 安装 Hugo V0.93.2"
---
+++ webs/notary/toto.sh
@@ -0,0 +1,44 @@
+workdir=$1
+initdir=$2
+
+source ${initdir}/libs/common.sh
+
+before_notary_website(){
+    install_huinstall_hugo_v83_1go_v114
+
+    install_postcss
+
+    # 添加网站访问统计
+    echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="e2245cb1-9c47-4c63-9330-88f7e729f480"></script>' >>  ./layouts/partials/favicons.html
+
+}
+
+after_notary_website(){
+
+    #npm run build:production
+
+    mkdir output
+    hugo \
+    --destination ./output \
+    --cleanDestinationDir \
+    --minify \
+    --gc \
+    --enableGitInfo \
+    --baseURL https://notary.cncfstack.com
+
+}
+
+save_return(){
+    ls -lha
+    echo "${workdir}/output&oss://cncfstack-notary" > ${workdir}/ret-data
+}
+
+
+cd $workdir
+if cat .git/config  |grep '/notaryproject/notaryproject.dev.git' ;then
+    echo "=============================================> 匹配到 notary"
+    before_notary_website
+    after_notary_website
+    find_and_sed_v2 "./output"
+    save_return 
+fi
