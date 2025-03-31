9153c90bdc520efae1f8dccd29611ccbf4777676
weyu <985695055@qq.com>
2025-03-31 11:45:49 +0800

    fdsa

--- libs/common.sh
+++ libs/common.sh
@@ -1,6 +1,4 @@
 
-set -x
-
 # 定义颜色变量
 BLACK='\033[0;30m'
 RED='\033[0;31m'
@@ -10,7 +8,7 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 ############################################################
 # log output
@@ -32,7 +30,7 @@ log_error(){
 
 install_hugo(){
 
-    if [ -z $1 ];then
+    if [ -z "$1" ];then
         hugo_url="https://github.com/gohugoio/hugo/releases/download/v0.133.0/hugo_extended_0.133.0_linux-amd64.tar.gz"
     else
         hugo_url=$1
@@ -41,108 +39,145 @@ install_hugo(){
     pkg_name=`echo $hugo_url|awk -F'/' '{print $NF}'`
 
     log_info "=============================================> 下载并安装 hugo: $pkg_name"
-    wget -q -O $hugo_url
+    wget -q  $hugo_url -O $pkg_name
     tar xf $pkg_name
 
     sudo cp ./hugo /usr/bin/
     sudo chmod +x /usr/bin/hugo
 
-    if [ -x /usr/bin/hugo ];then
+    if [ ! -x "/usr/bin/hugo" ];then
         log_error "=============================================>  hugo 安装失败: /usr/bin/hugo 文件不存在或者没有成功设置可执行权限"
     fi
 }
 
-install_hugo_v65_3(){  install_hugo https://github.com/gohugoio/hugo/releases/download/v0.65.3/hugo_extended_0.65.3_Linux-64bit.tar.gz }
-install_hugo_v66(){    install_hugo https://github.com/gohugoio/hugo/releases/download/v0.66.0/hugo_extended_0.66.0_Linux-64bit.tar.gz }
-install_hugo_v68_3(){  install_hugo https://github.com/gohugoio/hugo/releases/download/v0.68.3/hugo_extended_0.68.3_Linux-64bit.tar.gz }
-install_hugo_v80(){    install_hugo https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz }
-install_hugo_v83_1(){  install_hugo https://github.com/gohugoio/hugo/releases/download/v0.83.1/hugo_extended_0.83.1_Linux-64bit.tar.gz }
-install_hugo_v93_2(){  install_hugo https://github.com/gohugoio/hugo/releases/download/v0.93.2/hugo_extended_0.93.2_Linux-64bit.tar.gz }
-install_hugo_v99_1(){  install_hugo https://github.com/gohugoio/hugo/releases/download/v0.99.1/hugo_extended_0.99.1_Linux-64bit.tar.gz }
-install_hugo_v100_2(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.100.2/hugo_extended_0.100.2_Linux-64bit.tar.gz }
-install_hugo_v102_3(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_extended_0.102.3_Linux-64bit.tar.gz }
-install_hugo_v108(){   install_hugo https://github.com/gohugoio/hugo/releases/download/v0.108.0/hugo_extended_0.108.0_Linux-64bit.tar.gz }
-install_hugo_v111_3(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.tar.gz }
-install_hugo_v114(){   install_hugo https://github.com/gohugoio/hugo/releases/download/v0.114.0/hugo_extended_0.114.0_linux-amd64.tar.gz }
-install_hugo_v120(){   install_hugo https://github.com/gohugoio/hugo/releases/download/v0.120.0/hugo_extended_0.120.0_linux-amd64.tar.gz }
-install_hugo_v122(){   install_hugo https://github.com/gohugoio/hugo/releases/download/v0.122.0/hugo_extended_0.122.0_linux-amd64.tar.gz }
-install_hugo_v124_1(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.124.1/hugo_extended_0.124.1_linux-amd64.tar.gz }
-install_hugo_v136_5(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.136.5/hugo_extended_0.136.5_linux-amd64.tar.gz }
-install_hugo_v139_3(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.139.3/hugo_extended_0.139.3_linux-amd64.tar.gz }
-install_hugo_v143_1(){ install_hugo https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_extended_0.143.1_linux-amd64.tar.gz }
-install_hugo_v145(){   install_hugo https://github.com/gohugoio/hugo/releases/download/v0.145.0/hugo_extended_0.145.0_linux-amd64.tar.gz }
-
-
-
-install_postcss(){
-    log_info "=============================================> 安装 postCSS"
-    npm i -D postcss postcss-cli autoprefixer
+install_hugo_v65_3(){  
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.65.3/hugo_extended_0.65.3_Linux-64bit.tar.gz" 
+}
+install_hugo_v66(){    
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.66.0/hugo_extended_0.66.0_Linux-64bit.tar.gz" 
+}
+install_hugo_v68_3(){  
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.68.3/hugo_extended_0.68.3_Linux-64bit.tar.gz" 
+}
+install_hugo_v80(){    
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz" 
+}
+install_hugo_v83_1(){  
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.83.1/hugo_extended_0.83.1_Linux-64bit.tar.gz" 
+}
+install_hugo_v93_2(){  
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.93.2/hugo_extended_0.93.2_Linux-64bit.tar.gz" 
+}
+install_hugo_v99_1(){  
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.99.1/hugo_extended_0.99.1_Linux-64bit.tar.gz" 
+}
+install_hugo_v100_2(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.100.2/hugo_extended_0.100.2_Linux-64bit.tar.gz" 
+}
+install_hugo_v102_3(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_extended_0.102.3_Linux-64bit.tar.gz" 
+}
+install_hugo_v108(){   
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.108.0/hugo_extended_0.108.0_Linux-64bit.tar.gz" 
+}
+install_hugo_v111_3(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.tar.gz" 
+}
+install_hugo_v114(){   
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.114.0/hugo_extended_0.114.0_linux-amd64.tar.gz" 
+}
+install_hugo_v120(){   
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.120.0/hugo_extended_0.120.0_linux-amd64.tar.gz" 
+}
+install_hugo_v122(){   
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.122.0/hugo_extended_0.122.0_linux-amd64.tar.gz" 
+}
+install_hugo_v124_1(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.124.1/hugo_extended_0.124.1_linux-amd64.tar.gz" 
+}
+install_hugo_v136_5(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.136.5/hugo_extended_0.136.5_linux-amd64.tar.gz" 
+}
+install_hugo_v139_3(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.139.3/hugo_extended_0.139.3_linux-amd64.tar.gz" 
+}
+install_hugo_v143_1(){ 
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_extended_0.143.1_linux-amd64.tar.gz" 
+}
+install_hugo_v145(){   
+    install_hugo "https://github.com/gohugoio/hugo/releases/download/v0.145.0/hugo_extended_0.145.0_linux-amd64.tar.gz" 
 }
 
 
-
-check_cdn_change(){
-    log_info "=============================================> 以下文件进行了 cdn 替换，请确认文件在 https://cdn.cncfstack.com 是否存在"
-    grep "cdn.cncfstack.com" ./* -R |grep -v "otocn\.sed"|awk -F':' '{print $1}'
-}
-
-
-check_not_change(){
-    log_info "=============================================> 以下可能的外部文件未被处理"
-    # 有些图标表情使用 CDN 的 SVG，这类也可以代理。TODO：有些svg中会包含地址，这类是不需要处理的，但是会grep出来。不移除会有大量的无效信息，还是不显示svg内容，根据实际情况单独处理
-    # raw.githubusercontent.com 是 github 的内容，太多输出了，有依赖单独处理吧
-    grep -iEo "(maxcdn.bootstrapcdn.com|code.jquery.com|cdnjs.cloudflare.com|cdn-images.mailchimp.com|cdn.jsdelivr.net|fonts.googleapis.com|unpkg.com|www.googletagmanager.com)" ./* -R |grep -vE "(\.sh\:|\.md\:|\.toml|index\.rss\.xml|README\.txt\:|otocn\.sed\:|\.svg\:|node_modules)" 
-}
+# install_postcss(){
+#     log_info "=============================================> 安装 postCSS"
+#     npm i -D postcss postcss-cli autoprefixer
+# }
 
 
-find_and_sed(){
-    # 查找可能存在外部地址的文件，
-    # 对于其他文件即使包含外部地址也不需要处理，比如 svg 图片中的google字体地址
-    find  . -type f -iname "*.txt" \
-        -o -iname "*.md" \
-        -o -iname "*.toml" \
-        -o -iname "*.js" \
-        -o -iname "*.mjs" \
-        -o -iname "*.html" \
-        -o -iname "*.css" \
-        -o -iname "*.sass" \
-        -o -iname "*.scss" \
-        -o -iname "*.tpl" \
-        -o -iname "*.rst" > ${workdir}/filelist
 
-    # 循环依次处理可能包含外部链接的文件，并进行替换
-    for file in `cat ${workdir}/filelist`
-    do
-        sudo sed -i -f ${initdir}/toto.sed $file
-    done
+# check_cdn_change(){
+#     log_info "=============================================> 以下文件进行了 cdn 替换，请确认文件在 https://cdn.cncfstack.com 是否存在"
+#     grep "cdn.cncfstack.com" ./* -R |grep -v "otocn\.sed"|awk -F':' '{print $1}'
+# }
 
-    check_cdn_change
-    check_not_change
-}
 
-# 可以指定处理的路径，这在构建完成后再进行替换时很有用
-find_and_sed_v2(){
-    path=$1
-    # 查找可能存在外部地址的文件，
-    # 对于其他文件即使包含外部地址也不需要处理，比如 svg 图片中的google字体地址
-    find  $path -type f -iname "*.txt" \
-        -o -iname "*.md" \
-        -o -iname "*.toml" \
-        -o -iname "*.js" \
-        -o -iname "*.mjs" \
-        -o -iname "*.html" \
-        -o -iname "*.css" \
-        -o -iname "*.sass" \
-        -o -iname "*.scss" \
-        -o -iname "*.tpl" \
-        -o -iname "*.rst" > ${workdir}/filelist
+# check_not_change(){
+#     log_info "=============================================> 以下可能的外部文件未被处理"
+#     # 有些图标表情使用 CDN 的 SVG，这类也可以代理。TODO：有些svg中会包含地址，这类是不需要处理的，但是会grep出来。不移除会有大量的无效信息，还是不显示svg内容，根据实际情况单独处理
+#     # raw.githubusercontent.com 是 github 的内容，太多输出了，有依赖单独处理吧
+#     grep -iEo "(maxcdn.bootstrapcdn.com|code.jquery.com|cdnjs.cloudflare.com|cdn-images.mailchimp.com|cdn.jsdelivr.net|fonts.googleapis.com|unpkg.com|www.googletagmanager.com)" ./* -R |grep -vE "(\.sh\:|\.md\:|\.toml|index\.rss\.xml|README\.txt\:|otocn\.sed\:|\.svg\:|node_modules)" 
+# }
 
-    # 循环依次处理可能包含外部链接的文件，并进行替换
-    for file in `cat ${workdir}/filelist`
-    do
-        sudo sed -i -f ${initdir}/toto.sed $file
-    done
 
-    check_cdn_change
-    #check_not_change
-}
+# find_and_sed(){
+#     # 查找可能存在外部地址的文件，
+#     # 对于其他文件即使包含外部地址也不需要处理，比如 svg 图片中的google字体地址
+#     find  . -type f \( -iname "*.txt" \
+#         -o -iname "*.md" \
+#         -o -iname "*.toml" \
+#         -o -iname "*.js" \
+#         -o -iname "*.mjs" \
+#         -o -iname "*.html" \
+#         -o -iname "*.css" \
+#         -o -iname "*.sass" \
+#         -o -iname "*.scss" \
+#         -o -iname "*.tpl" \
+#         -o -iname "*.rst" \) > ${workdir}/filelist
+
+#     # 循环依次处理可能包含外部链接的文件，并进行替换
+#     for file in `cat ${workdir}/filelist`
+#     do
+#         sudo sed -i -f ${initdir}/toto.sed $file
+#     done
+
+#     check_cdn_change
+#     check_not_change
+# }
+
+# # 可以指定处理的路径，这在构建完成后再进行替换时很有用
+# find_and_sed_v2(){
+#     path=$1
+#     # 查找可能存在外部地址的文件，
+#     # 对于其他文件即使包含外部地址也不需要处理，比如 svg 图片中的google字体地址
+#     find  $path -type f -iname "*.txt" \
+#         -o -iname "*.md" \
+#         -o -iname "*.toml" \
+#         -o -iname "*.js" \
+#         -o -iname "*.mjs" \
+#         -o -iname "*.html" \
+#         -o -iname "*.css" \
+#         -o -iname "*.sass" \
+#         -o -iname "*.scss" \
+#         -o -iname "*.tpl" \
+#         -o -iname "*.rst" > ${workdir}/filelist
+
+#     # 循环依次处理可能包含外部链接的文件，并进行替换
+#     for file in `cat ${workdir}/filelist`
+#     do
+#         sudo sed -i -f ${initdir}/toto.sed $file
+#     done
+
+#     check_cdn_change
+#     #check_not_change
+# }
