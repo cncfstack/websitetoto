d49506f48c5ea5871ebaff73882aacb2dd2e4f96
weyu <985695055@qq.com>
2025-03-31 15:02:52 +0800

    fix dragonfly

--- webs/dragonfly/toto.sh
+++ webs/dragonfly/toto.sh
@@ -33,10 +33,10 @@ before_dragonfly(){
 #sed -i "s|plugins:\s*\[|plugins: [()=>({name:'umami-analytics',injectHtmlTags:()=>({headTags:[{tagName:'script',attributes:{defer:true,src:'https://umami.cncfstack.com/script.js','data-website-id':'e560133a-5a27-40ad-b816-9896199ffb01'}}]})}),|g" docusaurus.config.js
     
     log_info "=============================================> 配置文件中添加 umami"
-    sed -ri "s|plugins:\s*\[|plugins: [()=>({name:'umami-analytics',injectHtmlTags:()=>({headTags:[{tagName:'script',attributes:{defer:true,src:'https://umami.cncfstack.com/script.js','data-website-id':'c2f7c701-67da-49ac-9baa-f0daad6b1902'}}]})}),|g" docusaurus.config.ts
+    sed -ri "s|plugins:\s*\[|plugins: [()=>({name:'umami-analytics',injectHtmlTags:()=>({headTags:[{tagName:'script',attributes:{defer:true,src:'https://umami.cncfstack.com/script.js','data-website-id':'c2f7c701-67da-49ac-9baa-f0daad6b1902'}}]})}),|g" docusaurus.config.js
     
-    log_info "=============================================> ./docusaurus.config.ts 配置文件内容"
-    cat ./docusaurus.config.ts
+    log_info "=============================================> ./docusaurus.config.js 配置文件内容"
+    cat ./docusaurus.config.js
 
 }
 
@@ -47,7 +47,6 @@ after_dragonfly(){
 
     log_info "=============================================> 当前目录中文件列表"
     ls -lh
-
 }
 
 
