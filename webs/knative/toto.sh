ea5a1dd98e4c0b31834bc7ef5cfe37182d322e39
weyu <985695055@qq.com>
2025-03-31 20:55:49 +0800

    add knative

---
+++ webs/knative/toto.sh
@@ -0,0 +1,37 @@
+workdir=$1
+initdir=$2
+
+set -x
+
+source ${initdir}/libs/common.sh
+
+
+before_knative(){
+    
+    ./hack/build.sh
+
+}
+
+after_knative(){
+
+    log_info "=============================================> 当前目录中文件列表"
+    ls -lh
+
+    log_info "=============================================> site"
+    ls -lh ./site
+
+}
+
+
+save_return(){
+    echo "${workdir}/site&oss://cncfstack-knative" > ${workdir}/ret-data
+}
+
+
+if cat .git/config  |grep '/knative/docs.git' ;then
+    log_info "=============================================> 匹配到 knative"
+    before_knative
+    after_knative
+    find_and_sed_v2 "./build"
+    save_return 
+fi
