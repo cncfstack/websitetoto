755a82a6275509806186d09b5769855ed9a247e4
weyu <985695055@qq.com>
2025-04-22 22:56:48 +0800

    kubeedge

--- webs/kubeedge/toto.sh
+++ webs/kubeedge/toto.sh
@@ -1,16 +1,12 @@
-workdir=$1
-initdir=$2
-
 source libs/common.sh
 
-
-before_kubeedge(){
+before_build(){
     npm install
     sed -i "s|plugins:\s*\[|plugins: [()=>({name:'umami-analytics',injectHtmlTags:()=>({headTags:[{tagName:'script',attributes:{defer:true,src:'https://umami.cncfstack.com/script.js','data-website-id':'9e00beea-6f49-4460-b456-b4fc5fb216c5'}}]})}),|g" docusaurus.config.js
     cat ./docusaurus.config.js
 }
 
-after_kubeedge(){
+build(){
     echo "npm build-----"
 
     npm run build
@@ -21,16 +17,30 @@ after_kubeedge(){
 
 
 save_return(){
-    echo "${workdir}/build&oss://cncfstack-kubeedge" > ${workdir}/ret-data
+    # echo "${workdir}/build&oss://cncfstack-kubeedge" > ${workdir}/ret-data
+    # 这行很重要，在其他关联项目中，文件名称必须要匹配
+    tarfile="kubeedge.tgz"
+
+    # 进入到site目录后进行打包，这样是为了便于部署时解压
+    tar -czf ${tarfile} -C build .
+
+    if [ ! -s ${tarfile} ];then
+        log_error "站点构建失败"
+    fi
+
+    debug_tools
+    
+    log_info "站点构建完成"
+
+    echo "project_dir/${tarfile}" > ret-data
 }
 
 
-cd $workdir
-
+cd project_dir
 if cat .git/config  |grep '/kubeedge/website.git' ;then
-    echo "=============================================> 匹配到 kubeedge"
-    before_kubeedge
-    find_and_sed
-    after_kubeedge
+    echo " 匹配到 kubeedge"
+    before_build
+    find_and_sed_v2 "./build"
+    build
     save_return 
 fi
