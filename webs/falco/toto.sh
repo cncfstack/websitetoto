90bda38d4048dace9c1984868ec593b58e8d345a
weyu <985695055@qq.com>
2025-04-22 17:06:32 +0800

    falco

--- webs/falco/toto.sh
+++ webs/falco/toto.sh
@@ -1,9 +1,6 @@
-workdir=$1
-initdir=$2
-
 source libs/common.sh
 
-before_falco_website(){
+before_build(){
     install_hugo_v108
     install_postcss
     make dependencies
@@ -13,10 +10,11 @@ before_falco_website(){
 
 }
 
-after_falco_website(){
+build(){
 
     #npm run build:production
 
+    log_info "开始进行网站构建"
     mkdir output
     hugo \
     --destination ./output \
@@ -24,21 +22,37 @@ after_falco_website(){
     --minify \
     --gc \
     --enableGitInfo \
-    --baseURL https://falco.cncfstack.com
+    --baseURL https://falco.website.cncfstack.com
 
 }
 
 save_return(){
-    ls -lha
-    echo "${workdir}/output&oss://cncfstack-falco" > ${workdir}/ret-data
+    # ls -lha
+    # echo "${workdir}/output&oss://cncfstack-falco" > ${workdir}/ret-data
+
+    # 这行很重要，在其他关联项目中，文件名称必须要匹配
+    tarfile="falco.tgz"
+
+    # 进入到site目录后进行打包，这样是为了便于部署时解压
+    tar -czf ${tarfile} -C output .
+
+    if [ ! -s ${tarfile} ];then
+        log_error "站点构建失败"
+    fi
+
+    debug_tools
+    
+    log_info "站点构建完成"
+
+    echo "project_dir/${tarfile}" > ret-data
 }
 
 
-cd $workdir
+cd project_dir
 if cat .git/config  |grep '/falcosecurity/falco-website.git' ;then
-    echo "=============================================> 匹配到 falco"
-    before_falco_website
-    after_falco_website
+    echo "匹配到 falco"
+    before_build
+    build
     find_and_sed_v2 "./output"
     save_return 
 fi
