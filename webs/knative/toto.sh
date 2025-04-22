d325d7107e4de964cdc6fb0c23e2e1ac793bb7e3
weyu <985695055@qq.com>
2025-04-22 22:53:58 +0800

    knative

--- webs/knative/toto.sh
+++ webs/knative/toto.sh
@@ -1,18 +1,13 @@
-workdir=$1
-initdir=$2
-
-set -x
-
 source libs/common.sh
 
+before_build(){
 
-before_knative(){
     pip install -r requirements.txt
-
     bash -x ./hack/build.sh
+    
 }
 
-after_knative(){
+build(){
 
     log_info "=============================================> 当前目录中文件列表"
     ls -lh
@@ -26,15 +21,29 @@ after_knative(){
 
 
 save_return(){
-    echo "${workdir}/site&oss://cncfstack-knative" > ${workdir}/ret-data
+    #echo "${workdir}/site&oss://cncfstack-knative" > ${workdir}/ret-data
+    # 这行很重要，在其他关联项目中，文件名称必须要匹配
+    tarfile="knative.tgz"
+
+    # 进入到site目录后进行打包，这样是为了便于部署时解压
+    tar -czf ${tarfile} -C site .
+
+    if [ ! -s ${tarfile} ];then
+        log_error "站点构建失败"
+    fi
+
+    debug_tools
+    
+    log_info "站点构建完成"
+
+    echo "project_dir/${tarfile}" > ret-data
 }
 
-cd $workdir
-
+cd project_dir
 if cat .git/config  |grep '/knative/docs.git' ;then
-    log_info "=============================================> 匹配到 knative"
-    before_knative
-    after_knative
+    log_info "匹配到 knative"
+    before_build
+    build
     find_and_sed_v2 "./build"
     save_return 
 fi
