524a6a7341de7ac451b12c1460c2e8f01aed75d8
weyu <985695055@qq.com>
2025-04-22 15:18:55 +0800

    coredns3

--- webs/coredns/toto.sh
+++ webs/coredns/toto.sh
@@ -1,20 +1,19 @@
-workdir=$1
-
 source libs/common.sh
 
 before_build(){
     install_hugo_v100_2
     install_postcss
 
-    # 添加网站访问统计
+    log_info "添加网站访问统计"
     echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="9f78fcf9-90dc-4597-8fb8-a2b26aaf7656"></script>' >>  layouts/partials/favicons.html
-
 }
+
 
 build(){
 
     #npm run build:production
 
+    log_info "开售构建站点"
     mkdir output
     hugo \
     --destination ./output \
@@ -29,11 +28,11 @@ build(){
 save_return(){
 
     #echo "${workdir}/output&oss://cncfstack-coredns" > ${workdir}/ret-data
-    # 这行很重要，在其他关联项目中，文件名称必须要匹配
+    log_info "这行很重要，在其他关联项目中，文件名称必须要匹配"
     tarfile="coredns.tgz"
 
-    # 进入到site目录后进行打包，这样是为了便于部署时解压
-    tar -czf ${tarfile} -C ${workdir}/output .
+    log_info "进入到site目录后进行打包，这样是为了便于部署时解压"
+    tar -czf ${tarfile} -C project_dir/output .
 
     if [ ! -s ${tarfile} ];then
         log_error "站点构建失败"
@@ -43,11 +42,11 @@ save_return(){
     
     log_info "站点构建完成"
 
-    echo "${workdir}/${tarfile}" > ${workdir}/ret-data
+    echo "project_dir/${tarfile}" > project_dir/ret-data
 }
 
 
-cd $workdir
+cd project_dir
 if cat .git/config  |grep '/coredns/coredns.io.git' ;then
     echo "=============================================> 匹配到 coredns"
     before_build
@@ -55,3 +54,4 @@ if cat .git/config  |grep '/coredns/coredns.io.git' ;then
     find_and_sed_v2 "./output"
     save_return 
 fi 
+cd -
