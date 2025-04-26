40e271880e97d10bb3c59459256c0b704315ca04
weyu <985695055@qq.com>
2025-04-26 18:10:00 +0800

    spiffe fix2

--- webs/spiffe/toto.sh
+++ webs/spiffe/toto.sh
@@ -3,9 +3,9 @@ source libs/common.sh
 before_build(){
 
     # install_hugo_v68_3
-    install_hugo_v99_1
+    install_hugo_v68_3
     npm install
-    python pull_external.py
+    # python pull_external.py
 
     # 添加网站访问统计
     echo '<script defer src="https://umami.cncfstack.com/script.js" data-website-id="86f12ca0-83a8-470a-af6c-6ba96d72998e"></script>' >>  layouts/partials/meta.html
@@ -16,16 +16,17 @@ build(){
 
     #npm run build:production
 
-    mkdir output
-    hugo \
-    --destination ./output \
-    --cleanDestinationDir \
-	--buildDrafts \
-	--buildFuture \
-    --minify \
-    --gc \
-    --enableGitInfo \
-    --baseURL https://spiffe.website.cncfstack.com
+    # mkdir public
+    # hugo \
+    # --destination ./public \
+    # --cleanDestinationDir \
+	# --buildDrafts \
+	# --buildFuture \
+    # --minify \
+    # --gc \
+    # --enableGitInfo \
+    # --baseURL https://spiffe.website.cncfstack.com
+    make production-build
 
 }
 
@@ -35,7 +36,7 @@ save_return(){
     tarfile="spiffe.tgz"
 
     # 进入到site目录后进行打包，这样是为了便于部署时解压
-    tar -czf ${tarfile} -C output .
+    tar -czf ${tarfile} -C public .
 
     if [ ! -s ${tarfile} ];then
         log_error "站点构建失败"
@@ -50,7 +51,7 @@ save_return(){
 }
 
 after_build(){
-    filetoto "./output"
+    filetoto "./public"
     save_return
 }
 
