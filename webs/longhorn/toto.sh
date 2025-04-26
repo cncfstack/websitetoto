bd09c131c20b0e5eb02e4dc236a733304339ebd6
weyu <985695055@qq.com>
2025-04-26 16:22:37 +0800

    longhorn

--- webs/longhorn/toto.sh
+++ webs/longhorn/toto.sh
@@ -44,12 +44,15 @@ save_return(){
     echo "project_dir/${tarfile}" > ret-data
 }
 
+after_build(){
+    filetoto "./output"
+    save_return
+}
 
 cd project_dir
 if cat .git/config  |grep '/longhorn/website.git' ;then
     echo "匹配到 longhorn"
     before_build
     build
-    find_and_sed_v2 "./output"
-    save_return 
+    after_build
 fi
